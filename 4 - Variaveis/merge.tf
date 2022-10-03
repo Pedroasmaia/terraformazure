6761432d575c48957d04d399a811a71df7f20170
Pedro Maia <pedro.maia@ivoryit.com.br>
2022-10-03 15:41:17 -0300

    removendo merge.tf

--- 4 - Variaveis/merge.tf
+++
@@ -1,9 +0,0 @@
-#! Juntar valores no terraform usando o merge()
-resource "azurerm_resource_group" "rg-merge" {
-  name = "rg-merge"
-  location = var.localtion
-  tags = merge(var.tags,{
-    "plataforma" : "udemy"
-    "curso" : "terraform"
-  })
-}
