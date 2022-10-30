267f66a5a9291abce5b4b32e1e990d3f8dc91a7d
Pedro Maia <pedro.maia@ivoryit.com.br>
2022-10-30 17:36:57 -0300

    Finalização do Modulo 15

--- N - Máquinas Virtuais/vmlinux.tf
+++ N - Máquinas Virtuais/vmlinux.tf
@@ -1,22 +1,25 @@
-resource "azurerm_windows_virtual_machine" "vmwin" {
-  name                = "vm"
+resource "azurerm_linux_virtual_machine" "vmlinux" {
+  name                = "vmlinux"
   resource_group_name = azurerm_resource_group.rgvmtf.name
   location            = azurerm_resource_group.rgvmtf.location
 
 
   size                  = "Standard_B1ls"
-  admin_username        = "L1ef"
+  admin_username        = "l1eflinux"
   admin_password        = "myp@assw@rd"
   network_interface_ids = [azurerm_network_interface.nic-teste.id]
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
-  source_image_reference {
-    publisher = "MicrosoftWindosServer"
-    offer     = "WindowsServer"
-    sku       = "2016-Datacenter"
+  admin_ssh_key {
+    username   = "l1eflinux"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+ source_image_reference {
+    publisher = "Cononical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
     version   = "lastet"
   }
-
 }
